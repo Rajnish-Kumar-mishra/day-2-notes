1ee5a9b9021180048b9d7cb5f434b135a23a2ce6
Rajnish-Kumar-mishra <rjnish.047@gmail.com>
2020-03-25 13:12:07 +0000

    new notes

---
+++ d2n.py
@@ -0,0 +1,22 @@
+cd to any folder 
+__________
+ 
+git clone  url name of any repositry
+
+________________
+
+ceate any  file in that repository 
+ ______________
+
+git add -A 
+#to add files
+__________
+git status
+#to track files
+____________
+git commit -m "any message"
+
+_______________
+git push to upload file in that repository
+ 
+_____________
